bb705fc918278ff1cc6b56d27b2987ee868fbcb2
vnosinov <nosinov@gmail.com>
2021-02-21 15:28:00 +0200

    hw3_5 add file main.py

---
+++ hw3_5/main.py
@@ -0,0 +1,12 @@
+number1 = 20
+number2 = 30
+
+sum = number1 + number2 
+composition = number1 * number2 
+difference  = number1 - number2 
+relations = number1 / number2 
+
+print ("Sum =", sum)
+print ("Composition =", composition)
+print ("Difference =", difference)
+print ("Relations =", relations)
