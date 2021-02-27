89b3a7333b93b7ea6dda48451f16ed0b03aca12d
vladimir <nosinov@gmail.com>
2021-02-27 13:53:24 +0200

    add task hw6_1

---
+++ hw6_1/hw6_1.py
@@ -0,0 +1,27 @@
+while True:
+    try:
+        number_1 = int(input('Input number 1:  '))
+        number_2 = int(input('Input number 2:  '))
+    except ValueError:
+        print('Please< enter only numbers!')
+        exit()
+    const_opr = ('+', '-', '/', '*')
+    operand = input('Input operation:  ')
+    if operand in const_opr:
+        if operand == '+':
+            print(number_1, '+', number_2, '=', number_1 + number_2)
+        elif operand == '-':
+            print(number_1, '-', number_2, '=', number_1 - number_2)
+        elif operand == '*':
+            print(number_1, '*', number_2, '=', number_1 * number_2)
+        elif operand == '/' and number_2 == 0:
+            print('division by zero is prohibited')
+        else:
+            print(number_1, '/', number_2, '=', number_1 / number_2)
+    else:
+        print('not valid operator')
+
+
+
+
+
