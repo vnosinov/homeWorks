5d60c354ed476c39097312d4aab777b17cc42716
vnosinov <nosinov@gmail.com>
2021-03-05 14:37:18 +0200

    add task hw6_1  new version with functions

---
+++ hw6_1/calc_functions.py
@@ -0,0 +1,38 @@
+# file with functions for the calculator
+def c_sum(a, b):
+    """
+    the sum of to numbers
+    :param a: number 1
+    :param b: number 2
+    :return: sum
+    """
+    return a + b
+
+
+def c_diff(a, b):
+    """
+    difference of two numbers
+    :param a: number 1
+    :param b: number 2
+    :return: diff
+    """
+    return a - b
+
+
+def c_mult(a, b):
+    """
+    multiplication of two numbers
+    :param a: number 1
+    :param b: number 2
+    :return: multiplication
+    """
+    return a * b
+
+def c_div(a, b):
+    """
+    division of two numbers
+    :param a: number 1
+    :param b: number 2
+    :return: division
+    """
+    return a / b
--- hw6_1/hw6_1.py
+++ hw6_1/hw6_1.py
@@ -1,22 +1,40 @@
+import calc_functions as cf
+
+const_opr = ('+', '-', '/', '*')
 while True:
+
     try:
         number_1 = int(input('Input number 1:  '))
+    except ValueError:
+        print('Please< enter only numbers!')
+        continue
+
+    operand = input('Input operation:  ')
+    if operand not in const_opr:
+        print('not valid operator')
+        continue
+
+    try:
         number_2 = int(input('Input number 2:  '))
     except ValueError:
         print('Please< enter only numbers!')
-        exit()
-    const_opr = ('+', '-', '/', '*')
-    operand = input('Input operation:  ')
-    if operand in const_opr:
-        if operand == '+':
-            print(number_1, '+', number_2, '=', number_1 + number_2)
-        elif operand == '-':
-            print(number_1, '-', number_2, '=', number_1 - number_2)
-        elif operand == '*':
-            print(number_1, '*', number_2, '=', number_1 * number_2)
-        elif operand == '/' and number_2 == 0:
-            print('division by zero is prohibited')
-        else:
-            print(number_1, '/', number_2, '=', number_1 / number_2)
+        continue
+
+    if operand == '+':
+        print(cf.c_sum(number_1, number_2))
+    elif operand == '-':
+        print(cf.c_diff(number_1, number_2))
+    elif operand == '*':
+        print(cf.c_mult(number_1, number_2))
+    elif operand == '/' and number_2 == 0:
+        print('division by zero is prohibited')
     else:
-        print('not valid operator')
+        print(cf.c_div(number_1, number_2))
+
+
+
+
+
+
+
+
