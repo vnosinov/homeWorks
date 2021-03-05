0debfb6e5bc9525b12120b618714ded2f1d0aadb
vnosinov <nosinov@gmail.com>
2021-03-05 18:06:02 +0200

    add task hw6_4  new version

--- hw6_3/hw6_3.py
+++ hw6_3/hw6_3.py
@@ -4,10 +4,7 @@ for number in numbers:
     if number != 0:
         for candidate in range(2 * number, n+1, number):
             numbers[candidate-2] = 0
-result = []
-i = 0
-while i < len(numbers):
-    if numbers[i] != 0:
-        result.append(numbers[i])
-    i += 1
-print(result)
+print(list(filter(None, numbers)))
+
+
+
--- hw6_4/hw6_4.py
+++ hw6_4/hw6_4.py
@@ -4,17 +4,25 @@ while True:
         year = int(input('Input number: '))
     except ValueError:
         print('this is not a number')
-        exit()
+        continue
+
+    # if year % 4 != 0:
+    #     print('this is a noy leap year')
+    # elif year % 100 == 0:
+    #     if year % 400 == 0:
+    #         print('this is a leap year')
+    #     else:
+    #         print('this is a not leap year')
+    # else:
+    #     print('this is a leap year')
 
     if year % 4 != 0:
-        print('this is a noy leap year')
-    elif year % 100 == 0:
-        if year % 400 == 0:
-            print('this is a leap year')
-        else:
-            print('this is a not leap year')
+        print('this is a noy leap year', '1')
+    elif year % 100 == 0 and year % 400 != 0:
+        print('this is a noy leap year', '2')
     else:
-        print('this is a leap year')
+        print('this is leap year', '3')
+
 
 
 
