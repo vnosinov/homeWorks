7b2c2b1dd1df665e3bde586ae7db8d639b99e399
vnosinov <nosinov@gmail.com>
2021-03-09 23:09:34 +0200

    add tasks 8.4  new function version

--- hw8/hw8_4.py
+++ hw8/hw8_4.py
@@ -2,13 +2,18 @@ import os
 
 
 def recurs_dir(my_path):
+    result = []
     for i in os.listdir(my_path):
-        if os.path.isfile(my_path+'/'+i):
-            print(my_path+'/'+i)
+        path = os.path.join(my_path, i)
+        if not os.path.isdir(path):
+            result.append(path)
         else:
-            recurs_dir(my_path+'/'+i)
+            result += recurs_dir(path)
+    return result
 
 
 if __name__ == '__main__':
     my_path = '/home/vladimir/python'
-    recurs_dir(my_path)
+    my_list = recurs_dir(my_path)
+    for item in my_list:
+        print(item)
